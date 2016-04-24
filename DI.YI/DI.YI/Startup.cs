d013cfc1b948926ab60028c3b53ee21af0918431
348282932 <maxlong.live@icloude.com>
2016-04-24 16:28:13 +0800

    初始化 提交

---
+++ DI.YI/DI.YI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DI.YI.Startup))]
+namespace DI.YI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
